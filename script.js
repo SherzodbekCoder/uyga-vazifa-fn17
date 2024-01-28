83786ed268b92daa4e5726f64d8d31632f118aad
asosiymenu007 <asosiymenu007@gmail.com>
2024-01-28 18:03:40 +0500

    uyga vazifa

---
+++ script.js
@@ -0,0 +1,71 @@
+//1-miol 
+// function cub(agr1) {
+//     let s = agr1 ** 3;
+//     return Math.pow(agr1, 3);
+// }
+// console.log(cub(6));
+
+
+//3-misol
+// function oneNumber(n) {
+//     let arr = [];
+//     for (let i = 1; i <= n; i++) {
+//         arr.push(i);
+//     }
+//     return arr;
+// }
+
+
+//4-misol
+
+// function num(agr1) {
+//     let sum = 0;
+//     for (let i = 0; agr1; i++) {
+//         sum += agr1 % 10;
+//         agr1 = Math.floor(agr1 / 10);
+
+//     }
+//     return sum;
+// }
+// let arg1 = 123456
+// console.log(num(arg1));
+
+
+//6-misol
+
+// function randomSonlar(son1, son2) {
+//     return Math.trunc(Math.random() * (son2 - son1 + 1) + son1)
+// }
+// const randomNumber = randomSonlar(1, 10);
+// console.log(randomNumber);
+
+
+//7-misol
+// function topish(eni, boyi) {
+//     return eni * boyi
+// }
+// const hisoblash = topish(6, 10)
+// console.log(hisoblash);
+
+
+//8-misol
+// function func(gradus) {
+//     return (gradus - 32) * ( 1/8 );
+  
+//   }
+//   console.log(func(410));
+
+
+//10-misol
+
+// function min(a, b) {
+//     if (a < b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// }
+// console.log(min(3, 5));
+
+
+
